785ba27fdea79269b9b51049dd42ccc8af541346
Christopher Bley <chrisbley@gmail.com>
2015-08-22 18:57:32 -0400

    Style changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var update = require('./update');
 
-
-gulp.task('default', function(cb) {
-    update(cb);
+gulp.task('default', function (cb) {
+  update(cb);
 });
--- update.js
+++ update.js
@@ -5,94 +5,96 @@ var async = require('async');
 var unzip = require('unzip');
 
 var lastUpdateFile = './.lastUpdatedAt';
+var extractsBaseUrl = 'https://s3.amazonaws.com/metro-extracts.mapzen.com';
+var hbgArchive = 'harrisburg_pennsylvania.imposm-geojson.zip';
+var tempFile = './temp.zip';
 
 module.exports = update;
 
-function update(callback) {
+function update (callback) {
+  async.parallel(
+    [getLastUpdatedAtRemote, getLastUpdatedAtLocal],
+    function (err, results) {
+      if (err) throw err;
 
-    async.parallel([getLastUpdatedAtRemote, getLastUpdatedAtLocal], function(err, results) {
-        var remoteDate = new Date(results[0]);
-        var localDate = new Date(results[1]);
+      var remoteDate = new Date(results[0]);
+      var localDate = new Date(results[1]);
 
-        if (remoteDate > localDate) {
-            console.log('Updating data from Mapzen\'s metro extracts');
-            updateFromRemote(function(err) {
-                if (err) throw err;
+      if (remoteDate > localDate) {
+        console.log("Updating data from Mapzen's metro extracts");
+        updateFromRemote(function (err) {
+          if (err) throw err;
 
-                createLastUpdatedAtFile(remoteDate, callback);
-            });
-        } else {
-            console.log('Date is up-to-date.');
-            callback();
-        }
-    });
-}
-
-function updateFromRemote(callback) {
-    var hbgGeoJsonZipUrl = 'https://s3.amazonaws.com/metro-extracts.mapzen.com/harrisburg_pennsylvania.imposm-geojson.zip';
-    var outputFilename = './temp.zip';
-
-    request(hbgGeoJsonZipUrl)
-        .pipe(fs.createWriteStream(outputFilename))
-        .on('error', function(e) {
-            callback(e);
-        })
-        .on('close', function() {
-            fs.createReadStream(outputFilename)
-                .pipe(unzip.Extract({
-                    path: './data/'
-                }))
-                .on('close', function() {
-                    fs.unlinkSync(outputFilename);
-                    callback();
-                });
+          createLastUpdatedAtFile(remoteDate, callback);
         });
+      } else {
+        console.log('Date is up-to-date.');
+        callback();
+      }
+    }
+  );
 }
 
-function getLastUpdatedAtLocal(callback) {
-    fs.readFile(lastUpdateFile, 'utf-8', function(err, lastModified) {
-        if (err && err.code === 'ENOENT') {
-            createDefaultFile(function(err) {
-                if (err) callback(err);
+function updateFromRemote (callback) {
+  var hbgGeoJsonZipUrl = extractsBaseUrl + '/' + hbgArchive;
 
-                getLastUpdatedAtLocal(callback);
-            });
-        } else {
-            callback(err, lastModified);
-        }
-    });
-}
-
-function createDefaultFile(callback) {
-    console.log('Creating default lastUpdatedAt file');
-    createLastUpdatedAtFile(new Date(1983, 11, 26), callback);
-}
-
-function createLastUpdatedAtFile(date, callback) {
-    fs.writeFile(lastUpdateFile, date.toISOString(), callback);
-}
-
-function getLastUpdatedAtRemote(callback) {
-    var bucketMetadataUrl = 'https://s3.amazonaws.com/metro-extracts.mapzen.com';
-
-    request(bucketMetadataUrl, function(err, response, body) {
-        if (err)
-            callback(err);
-
-        xml2js.parseString(body, function(err, result) {
-            if (err)
-                callback(err);
-
-            var contents = result.ListBucketResult.Contents;
-            var match = contents.filter(matchesKey)[0];
-            var lastModified = String(match.LastModified);
-
-            callback(null, lastModified);
+  request(hbgGeoJsonZipUrl)
+    .pipe(fs.createWriteStream(tempFile))
+    .on('error', function (e) {
+      callback(e);
+    })
+    .on('close', function () {
+      fs.createReadStream(tempFile)
+        .pipe(unzip.Extract({
+          path: './data/'
+        }))
+        .on('close', function () {
+          fs.unlinkSync(tempFile);
+          callback();
         });
     });
 }
 
-function matchesKey(item) {
-    var key = 'LastUpdatedAt';
-    return String(item.Key) === key;
+function getLastUpdatedAtLocal (callback) {
+  fs.readFile(lastUpdateFile, 'utf-8', function (err, lastModified) {
+    if (err && err.code === 'ENOENT') {
+      createDefaultFile(function (err) {
+        if (err) callback(err);
+
+        getLastUpdatedAtLocal(callback);
+      });
+    } else {
+      callback(err, lastModified);
+    }
+  });
+}
+
+function createDefaultFile (callback) {
+  console.log('Creating default lastUpdatedAt file');
+  createLastUpdatedAtFile(new Date(1983, 11, 26), callback);
+}
+
+function createLastUpdatedAtFile (date, callback) {
+  fs.writeFile(lastUpdateFile, date.toISOString(), callback);
+}
+
+function getLastUpdatedAtRemote (callback) {
+  request(extractsBaseUrl, function (err, response, body) {
+    if (err) callback(err);
+
+    xml2js.parseString(body, function (err, result) {
+      if (err) callback(err);
+
+      var contents = result.ListBucketResult.Contents;
+      var match = contents.filter(matchesKey)[0];
+      var lastModified = String(match.LastModified);
+
+      callback(null, lastModified);
+    });
+  });
+}
+
+function matchesKey (item) {
+  var key = 'LastUpdatedAt';
+  return String(item.Key) === key;
 }
